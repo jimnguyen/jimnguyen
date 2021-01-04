5ad4e64a14493d3d990bba3425cbf1845e17a3db
Jim Nguyen <vjimnguyen@gmail.com>
2021-01-04 15:11:01 -0800

    Initialize amazon-clone and created header/navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNjzYk92VsQX6v71lWtO_KaVbBEFHZ5nc",
+  authDomain: "clone-4d425.firebaseapp.com",
+  projectId: "clone-4d425",
+  storageBucket: "clone-4d425.appspot.com",
+  messagingSenderId: "387513916348",
+  appId: "1:387513916348:web:7848aba3e6f005aac8e9b9",
+  measurementId: "G-V1T0H0ZX5W",
+};
