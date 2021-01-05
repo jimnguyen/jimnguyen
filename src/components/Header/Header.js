4a8ca5f8518662f9faeb6da5bfd5128c5e5541ff
Jim Nguyen <vjimnguyen@gmail.com>
2021-01-04 16:25:27 -0800

    Added home and navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
-import SearchIcon from "@material-ui/icons/Search";
+
+import { Search, ShoppingBasket } from "@material-ui/icons";
 
 function Header() {
   return (
@@ -12,23 +13,25 @@ function Header() {
       />
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        <SearchIcon className="header__searchIcon" />
+        <Search className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello User</span>
+          <span className="header__optionLineOne">Hello Guest</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
-          {" "}
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
         </div>
         <div className="header__option">
-          {" "}
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+      </div>
+      <div className="header__optionBasket">
+        <ShoppingBasket />
+        <span className="header__optionLineTwo header__basketCount">0</span>
       </div>
     </div>
   );
